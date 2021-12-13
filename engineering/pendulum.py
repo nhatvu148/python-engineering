58a07049753c3f46d41dd1422b21795777f5eaab
Nhật Vũ <nhatvu148@gmail.com>
2021-12-13 15:20:12 +0900

    fix pendulum

--- engineering/pendulum.py
+++ engineering/pendulum.py
@@ -17,10 +17,14 @@ def pend_motion(state, t):
 
 L = 5
 state0 = [pi/4, 0]
-time = arange(0, 60, 0.01)
+time_step = 0.01
+tf = 60
+time = arange(0, tf, time_step)
 
 soln = odeint(pend_motion, state0, time)
 disp = soln[:, 0]
+vel = soln[:, 1]
+
 x_coord = L*sin(disp)
 y_coord = -L*cos(disp)
 
@@ -29,13 +33,17 @@ ax = fig.add_subplot()
 ax.set_xlim(-L, L)
 ax.set_ylim(-L-1, 1)
 line, = ax.plot([], [], 'o-', lw=3)
+time_template = 'time = %.1fs'
+time_text = ax.text(2, 0, '')
+ax.grid()
 
 
 def pend_ani(i):
     thisx = [0, x_coord[i]]
     thisy = [0, y_coord[i]]
     line.set_data(thisx, thisy)
-    return line,
+    time_text.set_text(time_template % (i*time_step))
+    return line, time_text
 
 
 ani = animation.FuncAnimation(
